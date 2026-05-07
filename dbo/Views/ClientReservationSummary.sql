53d05b6e8637f2064568f2dcbd9415dec325662d
Toni-01 <antonjomullahaj@gmail.com>
2026-05-07 19:25:06 +0200

    feat: added views

---
+++ dbo/Views/ClientReservationSummary.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW ClientReservationSummary AS
+SELECT 
+    c.ClientID,
+    c.FirstName + ' ' + c.LastName AS ClientName,
+    COUNT(r.ReservationID) AS TotalReservations,
+    SUM(DATEDIFF(day, r.ArrivalDate, r.DepartureDate)) AS TotalNightsBooked,
+    SUM(r.TotalAmount) AS TotalAmountSpent,
+    MAX(r.ArrivalDate) AS LastArrivalDate,
+    STUFF((
+        SELECT ', ' + s.ServiceName
+        FROM ServiceBookings sb
+        JOIN Services s ON sb.ServiceID = s.ServiceID
+        WHERE sb.ReservationID = r.ReservationID
+        FOR XML PATH('')), 1, 2, '') AS ServicesUsed
+FROM 
+    Clients c
+INNER JOIN 
+    Reservations r ON c.ClientID = r.ClientID
+WHERE 
+    r.Status <> 'cancelled'
+GROUP BY 
+    c.ClientID, c.FirstName, c.LastName, r.ReservationID;
---
+++ dbo/Views/vw_RoomOccupancyStats.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW vw_RoomOccupancyStats AS  
+SELECT  
+    r.RoomType,  
+    COUNT(res.ReservationID) AS TotalBookings,  
+    AVG(DATEDIFF(day, res.ArrivalDate, res.DepartureDate)) AS AvgStayDuration,  
+    SUM(res.TotalAmount) AS TotalRevenue,  
+    COUNT(res.ReservationID) * 100.0 / (SELECT COUNT(*) FROM Reservations) AS BookingPercentage  
+FROM Rooms r  
+LEFT JOIN Reservations res ON r.RoomID = res.RoomID  
+GROUP BY r.RoomType;  
